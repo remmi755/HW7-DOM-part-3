33411bc0bddc865951c7764a50ec6f705de89d31
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-11-21 22:53:44 +0300

    add first part

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let text = document.querySelector('input');
+const btn = document.querySelector('button');
+const li = document.querySelector('li')
+
+function getText () {
+    text = this.value
+}
+function addText () {
+    li.innerText = 'text'
+}
+
