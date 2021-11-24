30419a35b21af4af11543bdcb21f595bcf492297
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-11-24 15:46:51 +0300

    add homework

--- scripts.js
+++ scripts.js
@@ -1,11 +1,45 @@
-let text = document.querySelector('input');
-const btn = document.querySelector('button');
-const li = document.querySelector('li')
+let inputUser = document.querySelector('input');
+const ul = document.querySelector('ul')
+let elem = document.getElementsByTagName('li')
+const body = document.getElementsByTagName('body')[0]
 
-function getText () {
-    text = this.value
+function createItem(value) {
+
+    return `<li class="li">${value}<button class="newBtn">${'Удалить'}</button></li>`
 }
-function addText () {
-    li.innerText = 'text'
+function createText(value) {
+
+    let list = ''
+    list = createItem(value)
+    return list
 }
+function clear(el){
+    el.remove()
+}
+
+function creatElement(){
+    elem = inputUser.value
+    ul.innerHTML += createText(elem)
+}
+
+body.addEventListener('click', function (event) {
+    let target = event.target
+
+        if(target.tagName === 'BUTTON' && target.className === 'add-new-str' && inputUser.value !== '') {
+            creatElement()
+            inputUser.value = ''
+        }
+
+        if(target.tagName === 'LI')  {
+            target.classList.toggle('active')
+        }
+
+       if(target.tagName === 'BUTTON' &&  target.className ==='newBtn') {
+
+           clear(target.closest('li'))
+        }
+  }
+)
+
+
 
